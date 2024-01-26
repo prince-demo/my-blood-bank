4e6b0922ac2e55050860b899852b83645a4e2d15
prince-demo <157696700+prince-demo@users.noreply.github.com>
2024-01-26 14:41:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ const port = process.env.PORT || 8080;
 //listen port
 app.listen(port, () => {
   console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+    `Server Running in  Mode on port ${process.env.PORT}`
       .bgCyan.white
   );
 });
